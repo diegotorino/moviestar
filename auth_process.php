592719c0dc176e5f10de4249c16ea143aca1b2c2
diegotorino <84508713+diegotorino@users.noreply.github.com>
2022-12-07 16:55:32 -0300

    moviestar

---
+++ auth_process.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("models/user.php");
+require_once("models/Message.php");
+require_once("models/userDAO.php");
+require_once("globals.php");
+require_once("db.php");
+
+$message = new Message($BASE_URL);
+
+
+// Resgata o tipo do formulário
+$type = filter_input(INPUT_POST, "type");
+
+
+
+//Verificação do tipo de formulário
+
+if($type === "register") {
+
+    $name = filter_input(INPUT_POST, "name");
+    $lastname = filter_input(INPUT_POST, "lastname");
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
+    $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+
+
+    //Verificação de dados minimos
+    if($name && $lastname && $email && $password) {
+
+
+
+
+    } else {
+
+        // enviar uma msg de erro dados faltantes
+
+
+
+
+    }
+
+} else if($ype === "login") {
+
+
+}
